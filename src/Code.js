4886addef0023323586c01c7f8804731cb1ef6b1
Thomas Metzger <tmp.1998@gmail.com>
2025-01-02 17:49:16 -0600

    chore(folder structure): improved folder structure

--- src/Code.js
+++ src/Code.js
@@ -3,64 +3,59 @@
 // https://medium.com/script-portal/how-to-create-web-app-with-google-apps-script-part-i-008b0ffc1adf
 // https://medium.com/google-cloud/easily-implementing-html-form-with-google-spreadsheet-as-database-using-google-apps-script-66472ab7bf6c
 
-const SPREADSHEET_ID = '1mO4clKaXB5KSpBq7cF8CDWtWiNadSouvTPqC83YhegY'
-const SHEET_NAME = 'Sheet2'
+const SPREADSHEET_ID = "1mO4clKaXB5KSpBq7cF8CDWtWiNadSouvTPqC83YhegY";
+const SHEET_NAME = "Sheet2";
 
 /**
  * Endoint router of the webapp. As of now, the only route served is the default. It serves index.html
  */
-function doGet() {
-  return HtmlService
-      .createTemplateFromFile('src/index')
-      .evaluate()
-      .setTitle("UDC signup form")
-      .setFaviconUrl('https://utiladivecenter.com/img/logo/UDC-LOGO-TINY.png')
+function doGet(e) {
+
+  let pageInfo = {
+    fileName: "src/index",
+    title: "UDC signup form"
+  }
+
+  if (e.pathInfo == 'admin') {
+    pageInfo.fileName = "src/reception";
+    pageInfo.title = "UDC admin reception form";
+  }
+
+
+  // var params = JSON.stringify(e);
+  // return ContentService.createTextOutput(params).setMimeType(ContentService.MimeType.JSON);
+
+  return HtmlService.createTemplateFromFile(pageInfo.fileName)
+    .evaluate()
+    .setTitle(pageInfo.title)
+    .setFaviconUrl("https://utiladivecenter.com/img/logo/UDC-LOGO-TINY.png");
 }
 
-
-/**
- * DEPRECATED
- */
-function doPost_old(formData) {
-  // const spreadsheetId = SpreadsheetApp.getActiveSpreadsheet().getId();
-  // const res = HtmlFormApp.appendFormData({ formData, spreadsheetId });
-  // console.log(ContentService.createTextOutput(
-  //   JSON.stringify({ message: "Done", row: res.range.getRow() })
-  // ))
-
-  console.log(formData)
-
-  return ContentService.createTextOutput("test"); // Please add this.
-
-}
-
-/**
- * TODO
- */
+// TODO
 function backendValidation() {}
 
-
-
 function processForm(formObject) {
-  console.log(formObject)
+  console.log(formObject);
 
-  const lock = LockService.getScriptLock()
-  lock.tryLock(10000)
+  const lock = LockService.getScriptLock();
+  lock.tryLock(10000);
 
   try {
-    const doc = SpreadsheetApp.openById(SPREADSHEET_ID)
+    const doc = SpreadsheetApp.openById(SPREADSHEET_ID);
 
-    console.log(doc.getName())
-    const sheet = doc.getSheetByName(SHEET_NAME)
+    console.log(doc.getName());
+    const sheet = doc.getSheetByName(SHEET_NAME);
 
-    const headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]
-    const nextRow = sheet.getLastRow() + 1
+    const headers = sheet
+      .getRange(1, 1, 1, sheet.getLastColumn())
+      .getValues()[0];
+    const nextRow = sheet.getLastRow() + 1;
 
-    const newRow = headers.map(header => {
-      return header === 'Date' ? new Date() : formObject[header]
-    })
+    const newRow = headers.map((header) => {
+      return header === "Date" ? new Date() : formObject[header];
+    });
 
-    sheet.getRange(nextRow, 1, 1, newRow.length).setValues([newRow])
+    sheet.getRange(nextRow, 1, 1, newRow.length).setValues([newRow]);
 
     // return ContentService
     //   .createTextOutput(JSON.stringify({ 'result': 'success', 'row': 'nextRow' }))
@@ -69,9 +64,9 @@ function processForm(formObject) {
     // return ContentService
     //   .createTextOutput(JSON.stringify({ 'result': 'error', 'error': e }))
     //   .setMimeType(ContentService.MimeType.JSON)
-    console.log(e)
+    console.log(e);
   } finally {
-    lock.releaseLock()
+    lock.releaseLock();
   }
 }
 
