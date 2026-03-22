2bb78eee9919174f9dd60b3c65cab1cfc15b2547
Thomas Metzger <tmp.1998@gmail.com>
2026-03-22 12:40:07 +0100

    :hammer: cleanup dev setup

--- api/index.js
+++ api/index.js
@@ -6,20 +6,9 @@ const app = express();
 
 const __dirname = import.meta.dirname;
 
-// Set EJS as the view engine
 app.set("view engine", "ejs");
-
-// Set views directory
 app.set("views", path.join(__dirname, "../views"));
 
-app.use(express.static(path.join(__dirname, "../public")));
-app.use(express.json());
-
-// Use routes
 app.use("/", indexRouter);
-
-// app.listen(3000, () => {
-//   console.log("Server is running on http://localhost:3000");
-// });
 
 export default app;
--- dev.js
+++ dev.js
@@ -1,6 +1,16 @@
+import express from "express";
+import path from "path";
 import app from "./api/index.js";
 
+const __dirname = import.meta.dirname;
+
+// Mimic Vercel cleanUrls + redirect locally
+const devApp = express();
+devApp.get("/", (req, res) => res.redirect(308, "/home"));
+devApp.use(express.static(path.join(__dirname, "public"), { extensions: ["html"] }));
+devApp.use(app);
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+devApp.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
