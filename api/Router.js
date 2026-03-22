abc26631fee18a899e59e8675ed016d1609a92fb
Thomas Metzger <tmp.1998@gmail.com>
2026-03-22 11:48:56 +0100

    :rocket: change deployment to serve static pages from cdn

--- api/Router.js
+++ api/Router.js
@@ -2,31 +2,6 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index", {
-    title: "Home Page",
-    page: "home",
-  });
-});
-
-router.get("/udc", (req, res) => {
-  const pkg = _sanitizePkg(req.query.pkg);
-  res.render("index", {
-    title: "UDC",
-    page: "udc",
-    pkg: pkg,
-  });
-});
-
-router.get("/liability", (req, res) => {
-  const pkg = _sanitizePkg(req.query.pkg);
-  res.render("index", {
-    title: "Liability Release",
-    page: "liability",
-    pkg: pkg,
-  });
-});
-
 router.get("/medical", (req, res) => {
   const pkg = _sanitizePkg(req.query.pkg);
   res.render("index", {
